f2df205b2db3638e126fc7d3a51fdb2fb91276f8
jinyy2 <jy5031le@gmail.com>
2020-08-15 23:27:00 +0900

    중복 순열

---
+++ Basic/src/PermutationWithRepetition.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class PermutationWithRepetition {
+	static int N, R;
+	static boolean[] visited;
+	static int[] number, input;
+	static int totalCount;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		number = new int[R];
+		input = new int[N];
+		totalCount = 0;
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < input.length; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		Permutation(0);
+		System.out.println("총 개수: " + totalCount);
+	}
+
+	private static void Permutation(int cnt) {
+		if (cnt == R) {
+			totalCount++;
+			System.out.println(Arrays.toString(number));
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			number[cnt] = input[i];
+			Permutation(cnt + 1);
+		}
+	}
+}
