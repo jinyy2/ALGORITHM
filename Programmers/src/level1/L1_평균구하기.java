3027802365caf0053608e9abf3c9c7bbe47f644a
jinyy2 <jy5031le@gmail.com>
2020-11-16 23:04:24 +0900

    평균 구하기

---
+++ Programmers/src/level1/L1_평균구하기.java
@@ -0,0 +1,15 @@
+package level1;
+
+public class L1_평균구하기 {
+	class Solution {
+		public double solution(int[] arr) {
+			double answer = 0;
+			int sum = 0;
+			for (int i = 0; i < arr.length; i++) {
+				sum += arr[i];
+			}
+			answer = (double) sum / arr.length;
+			return answer;
+		}
+	}
+}
