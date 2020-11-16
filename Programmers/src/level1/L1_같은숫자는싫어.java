d735ba78b8a7e4ef10f93a6f2e3ba1ef5cba31b9
jinyy2 <jy5031le@gmail.com>
2020-11-16 23:06:18 +0900

    같은 숫자는 싫어

--- Programmers/src/level1/L1_같은숫자는싫어.java
+++ Programmers/src/level1/L1_같은숫자는싫어.java
@@ -3,23 +3,25 @@ package level1;
 import java.util.*;
 
 public class L1_같은숫자는싫어 {
-	public int[] solution(int []arr) {
+	public class Solution {
+		public int[] solution(int[] arr) {
 
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(arr[0]);
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]==arr[i-1]) continue;
-            list.add(arr[i]);
-        }
-        int[] answer = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            answer[i] = list.get(i);
-        }
-        
-        
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+			ArrayList<Integer> list = new ArrayList<>();
+			list.add(arr[0]);
+			for (int i = 1; i < arr.length; i++) {
+				if (arr[i] == arr[i - 1])
+					continue;
+				list.add(arr[i]);
+			}
+			int[] answer = new int[list.size()];
+			for (int i = 0; i < list.size(); i++) {
+				answer[i] = list.get(i);
+			}
 
-        return answer;
-    }
+			// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+			System.out.println("Hello Java");
+
+			return answer;
+		}
+	}
 }
