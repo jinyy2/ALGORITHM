e353b7be6d9c946bf14e29eaaf4bd4fd722a0e5f
jinyy2 <jy5031le@gmail.com>
2020-11-17 22:48:20 +0900

    핸드폰 번호 가리기

---
+++ Programmers/src/level1/L1_핸드폰번호가리기.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class L1_핸드폰번호가리기 {
+
+	public static void main(String[] args) {
+		System.out.println("01033334444".substring(0));	
+		char[] c = "01033334444".toCharArray();
+		System.out.println(String.valueOf(c));
+	}
+
+}
