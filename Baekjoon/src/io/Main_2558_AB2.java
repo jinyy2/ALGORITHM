af8bdcb8a288e70faccc15410255b1be2585d596
jinyy2 <jy5031le@gmail.com>
2020-12-01 21:56:45 +0900

    입출력

---
+++ Baekjoon/src/io/Main_2558_AB2.java
@@ -0,0 +1,14 @@
+package io;
+
+import java.io.*;
+
+public class Main_2558_AB2 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		System.out.print(A + B);
+	}
+
+}
