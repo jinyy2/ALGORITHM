6c41f599f20d7527384b3c13d4a2d2ea49d146ba
jinyy2 <jy5031le@gmail.com>
2020-12-01 21:57:11 +0900

    입출력

---
+++ Baekjoon/src/io/Main_11021_AB7.java
@@ -0,0 +1,25 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11021_AB7 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		int T = Integer.parseInt(br.readLine());
+		int A = 0, B = 0;
+
+		for (int i = 1; i <= T; i++) {
+			st = new StringTokenizer(br.readLine());
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			System.out.println("Case #" + i + ": " + (A + B));
+		}
+
+	}
+
+}
