7c14ac8f6ef825523d4f523e199a1eae747e7188
jinyy2 <jy5031le@gmail.com>
2020-12-02 13:23:23 +0900

    입출력

--- Baekjoon/src/io/Main_1924_2007년.java
+++ Baekjoon/src/io/Main_1924_2007년.java
@@ -6,21 +6,21 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_1924_2007년 {
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int m = Integer.parseInt(st.nextToken());
 		int d = Integer.parseInt(st.nextToken());
 		int cnt = d;
-		int[] month = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-		String[] weekday = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-		
-		for(int i=0;i<m;i++) {
+		int[] month = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		String[] weekday = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+
+		for (int i = 0; i < m; i++) {
 			cnt += month[i];
 		}
-		System.out.println(weekday[cnt%7]);
-	
+		System.out.println(weekday[cnt % 7]);
+
 	}
 
 }
