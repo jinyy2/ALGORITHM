d0124d65b62218d0d9230c385d04d2f961e0a79f
jinyy2 <jy5031le@gmail.com>
2020-12-02 13:23:11 +0900

    입출력

--- Baekjoon/src/io/Main_8393_합.java
+++ Baekjoon/src/io/Main_8393_합.java
@@ -1,10 +1,21 @@
 package io;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main_8393_합 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
+		int sum = 0;
+
+		for (int i = 1; i <= N; i++) {
+			sum += i;
+		}
+		System.out.print(sum);
 	}
 
 }
