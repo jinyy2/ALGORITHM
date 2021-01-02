2cbfa5d3024a77ffafd3238b50ca490a962137a8
jinyy2 <jy5031le@gmail.com>
2021-01-02 20:07:28 +0900

    L1

---
+++ Programmers/src/level1/L1_짝수와홀수.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class L1_짝수와홀수 {
+
+	class Solution {
+	    public String solution(int num) {
+	        String answer = "";
+	        if(num%2==0) answer = "Even";
+	        else answer = "Odd";
+	        return answer;
+	    }
+	}
+
+}
