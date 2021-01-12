deba9e6b839e353ddf490ceec34be3d69a20a2f8
jinyy2 <jy5031le@gmail.com>
2021-01-12 22:59:06 +0900

    우선순위큐

---
+++ Baekjoon/src/Main_2075_N번째큰수.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main_2075_N번째큰수 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return -(o1 - o2);
+			}
+		});
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				pq.offer(Integer.parseInt(st.nextToken()));
+			}
+		}
+		int answer = 0;
+		for (int i = 0; i < N; i++) {
+			answer = pq.poll();
+		}
+		System.out.println(answer);
+
+	}
+
+}
