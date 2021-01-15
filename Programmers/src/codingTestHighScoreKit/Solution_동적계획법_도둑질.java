870de52e95c0f43cdcb1f27d922666606a5d277c
jinyy2 <jy5031le@gmail.com>
2021-01-15 16:52:43 +0900

    DP

---
+++ Programmers/src/codingTestHighScoreKit/Solution_동적계획법_도둑질.java
@@ -0,0 +1,26 @@
+package codingTestHighScoreKit;
+
+public class Solution_동적계획법_도둑질 {
+
+	public static void main(String[] args) {
+		int[] money = {1, 2, 3, 1};
+		System.out.println(solution(money));
+
+	}
+	 public static int solution(int[] money) {
+	        int answer = 0;
+	        int[][] dp = new int[2][money.length];
+	        dp[0][0] = money[0];
+	        dp[0][1] = money[0];
+	        dp[1][0] = 0;
+	        dp[1][1] = money[1];
+	        
+	        for(int i=2;i<money.length;i++){
+	            dp[0][i] = Math.max(dp[0][i-2]+money[i],dp[0][i-1]);
+	            dp[1][i] = Math.max(dp[1][i-2]+money[i],dp[1][i-1]);
+	        }
+	        answer = Math.max(dp[0][money.length-2],dp[1][money.length-1]);
+	        return answer;
+	    }
+
+}
