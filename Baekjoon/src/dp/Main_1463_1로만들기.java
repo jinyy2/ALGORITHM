b6adcba102e90fcc64018e8c19eeb87ad23e6e88
jinyy2 <jy5031le@gmail.com>
2021-01-17 21:57:58 +0900

    DP

---
+++ Baekjoon/src/dp/Main_1463_1로만들기.java
@@ -0,0 +1,37 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_1463_1로만들기 {
+	static int min;
+	static int[] dp;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		dp = new int[10000001];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		int N = Integer.parseInt(br.readLine());
+
+		dp(N, 0);
+		System.out.println(dp[1]);
+	}
+
+	private static void dp(int N, int cnt) {
+		if (dp[N] > cnt)
+			dp[N] = cnt;
+		else return;
+		
+		if (N == 1) {
+			return;
+		}
+		if (N % 3 == 0)
+			dp(N / 3, cnt + 1);
+		if (N % 2 == 0)
+			dp(N / 2, cnt + 1);
+		dp(N - 1, cnt + 1);
+	}
+
+}
