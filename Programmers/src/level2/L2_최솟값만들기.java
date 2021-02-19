d38dd35f323da6f15942869a817d577afa586935
jinyy2 <jy5031le@gmail.com>
2021-02-19 11:49:26 +0900

    L2

---
+++ Programmers/src/level2/L2_최솟값만들기.java
@@ -0,0 +1,23 @@
+package level2;
+
+import java.util.*;
+
+public class L2_최솟값만들기 {
+
+	public static void main(String[] args) {
+		int[] A = { 1, 4, 2 };
+		int[] B = { 5, 4, 4 };
+		System.out.println(solution(A, B));
+	}
+
+	public static int solution(int[] A, int[] B) {
+		int answer = 0;
+		Arrays.sort(A);
+		Arrays.sort(B);
+		for (int i = 0; i < A.length; i++) {
+			answer += A[i] * B[B.length - i - 1];
+		}
+		return answer;
+	}
+
+}
