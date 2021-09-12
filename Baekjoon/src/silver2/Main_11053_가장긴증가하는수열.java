29947895b8559daf22817cc2e04c8a0e67b6e3ad
jinyy2 <jy5031le@gmail.com>
2021-09-12 22:22:39 +0900

    가장 긴 증가하는 수열

---
+++ Baekjoon/src/silver2/Main_11053_가장긴증가하는수열.java
@@ -0,0 +1,33 @@
+package silver2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_11053_가장긴증가하는수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int result = 0;
+        for (int temp : dp) {
+            result = Math.max(result, temp);
+        }
+        System.out.println(result);
+    }
+}
