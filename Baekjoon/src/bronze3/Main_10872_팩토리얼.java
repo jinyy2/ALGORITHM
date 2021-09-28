7f1aaa93b828865b84f45dc4ac0ddf64f979b466
jinyy2 <jy5031le@gmail.com>
2021-09-28 19:22:32 +0900

    팩토리얼

---
+++ Baekjoon/src/bronze3/Main_10872_팩토리얼.java
@@ -0,0 +1,18 @@
+package bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_10872_팩토리얼 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(factorial(N));
+    }
+
+    public static int factorial(int n) {
+        if (n <= 1) return 1;
+        else return n * factorial(n - 1);
+    }
+}
