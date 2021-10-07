3d029820651ab1b8898fc39e28b087cb127771e5
jinyy2 <jy5031le@gmail.com>
2021-10-07 22:59:50 +0900

    피보나치 수 5

---
+++ Baekjoon/src/bronze2/Main_10870_피보나치수5.java
@@ -0,0 +1,19 @@
+package bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_10870_피보나치수5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[21];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[N]);
+    }
+}
