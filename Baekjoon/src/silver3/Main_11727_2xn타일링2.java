2f1d01c0f5ed2fcdf8fdec881c568f5a137429a6
jinyy2 <jy5031le@gmail.com>
2021-10-12 23:43:36 +0900

    2xn타일링2

--- Baekjoon/src/silver3/Main_11727_2xn타일링2.java
+++ Baekjoon/src/silver3/Main_11727_2xn타일링2.java
@@ -11,7 +11,7 @@ public class Main_11727_2xn타일링2 {
         int[] dp = new int[1001];
         dp[0] = 0;
         dp[1] = 1;
-        dp[2] = 2;
+        dp[2] = 3;
         for (int i = 3; i <= n; i++) {
             dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
         }
