09ba6524a8f5ec27b86fdab1dc5d90982435e2d3
jinyy2 <jy5031le@gmail.com>
2021-10-22 22:43:26 +0900

    큐 2

---
+++ Baekjoon/src/silver4/Main_18258_큐2.java
@@ -0,0 +1,56 @@
+package silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_18258_큐2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new LinkedList<>();
+        StringTokenizer st = null;
+        int N = Integer.parseInt(br.readLine());
+        int back = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            if (command.equals("push")) {
+                int value = Integer.parseInt(st.nextToken());
+                queue.offer(value);
+                back = value;
+            } else if (command.equals("pop")) {
+                if (queue.size() == 0) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else if (command.equals("size")) {
+                sb.append(queue.size()).append("\n");
+            } else if (command.equals("empty")) {
+                if (queue.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if (command.equals("front")) {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(queue.peek()).append("\n");
+                }
+            } else if (command.equals("back")) {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(back).append("\n");
+                }
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
+
